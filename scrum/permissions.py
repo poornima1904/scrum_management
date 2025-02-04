56e2d0c72d82ea979024153ca7bf5f6ae03112eb
poornima1904 <singhpoornima1904@gmail.com>
2025-02-04 09:43:06 +0530

    updated task permission

--- scrum/permissions.py
+++ scrum/permissions.py
@@ -1,5 +1,6 @@
 from rest_framework.permissions import BasePermission
 from .models import TeamMembership, Team, User
+from .utils.constants import SCRUM_MASTER
 
 
 class IsScrumMaster(BasePermission):
@@ -25,7 +26,7 @@ class IsScrumMasterOrAdminTeam(BasePermission):
         if request.method == 'GET':
               return True
 
-        if request.user.role == 'Scrum Master':
+        if request.user.role == SCRUM_MASTER:
             return True
 
         if request.method == 'POST':
@@ -50,19 +51,35 @@ class IsTeamAdminOrAssigneeOrReadOnly(BasePermission):
     - Team members can view tasks.
     """
 
+    def is_admin_or_parent_admin(self, team, user):
+        # Check if the user is an admin of the current team
+        team_membership = TeamMembership.objects.filter(team=team, user=user).first()
+        if team_membership and team_membership.role == 'admin':
+            return True
+
+        # If no admin role is found, check if there's a parent team and recursively check the parent team
+        parent_team = team.parent_team
+        if parent_team:
+            return self.is_admin_or_parent_admin(parent_team, user)
+
+        return False
+
     def has_object_permission(self, request, view, obj):
         user = request.user
+
+        if user.role == SCRUM_MASTER:
+            return True
 
         # Allow read-only access for team members
         if request.method in ['GET']:
             return obj.team.members.filter(id=user.id).exists()
 
-        # Team admins & Parent Team admins can view/assign tasks
-        if obj.team.is_admin_or_parent_admin(user):
-            return True
-
         # Assignee can update task status
-        if request.method in ['PATCH', 'PUT'] and obj.assigned_to == user:
-            return True
+        if request.method in ['PATCH', 'PUT']:
+            if obj.assigned_to == user:
+                return True
+
+            if self.is_admin_or_parent_admin(obj.team, user):
+                return True
 
         return False
